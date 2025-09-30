89e1c1efcecb245088083169c42b10c1d57c2ed7
Ben Haggard <21bhaggard@gmail.com>
2025-09-30 10:42:36 -0600

    build and deploy static site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  basePath: "/personal-website",
+  assetPrefix: "/personal-website/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
