35196442a10b8662b8129edf2af2635ea718eb9e
Ben Haggard <21bhaggard@gmail.com>
2026-02-09 20:43:59 -0700

    update work

--- src/components/sections/WorkExperienceSection.tsx
+++ src/components/sections/WorkExperienceSection.tsx
@@ -8,9 +8,9 @@ const workExperiences = [
     position: "Software Engineer",
     duration: "Jan 2025 – Current",
     responsibilities: [
-      "Redesigned cloud architecture using Docker, Terraform, and AWS (EB, EC2, VPC, IAM, Amplify, Cognito), improving reliability and reducing infrastructure costs by 50%.",
-      "Automated deployments with CI/CD pipelines to simplify future DevOps operations.",
-      "Migrated main website to Next.js with TypeScript and ShadCN, modernizing UI/UX",
+      "Redesigned cloud architecture integrating Docker, Terraform, and AWS (EB, EC2, VPC, IAM, Amplify, Cognito), improving reliability and reducing infrastructure costs by over 50%",
+      "Automated deployments with CI/CD pipelines using GitHub Actions, AWS CodePipeline, bash scripting, and Linux expertise to simplify future DevOps operations",
+      "Developed end-to-end application features: Python-based backend APIs, data pipelines (Airflow), and React/TypeScript frontend.",
     ],
   },
   {
@@ -19,19 +19,19 @@ const workExperiences = [
     position: "Frontend and QA Engineering Intern",
     duration: "Apr 2025 – Aug 2025",
     responsibilities: [
-      "Developed a unified React/TypeScript UI integrating internal npm packages, a browser extension, and multiple apps, simplifying cryptographic workflows, and improving secure content verification for users.",
-      "Collaborated with product design and engineering team to deliver accurate and on time feature releases.",
-      "Developed comprehensive unit tests for frontend React components using Vitest and Jest. Starting with the most business-critical components, I increased test coverage from 5% to 60%",
-      "Implemented some full-stack features including expanding support of signing and verifying web-based content by 200%",
+      "Developed a unified React/TypeScript UI integrating internal npm packages, a browser extension, and multiple apps, simplifying cryptographic workflows, and improving secure content verification for users",
+      "Collaborated with product design and engineering team to deliver accurate and on time feature releases",
+      "Developed comprehensive unit tests for frontend React components with Vitest and Jest. Starting with the most business-critical components, increased test coverage from 5% to 60%",
+      "Created full-stack features including expanding support of signing and verifying web-based content by 200%",
     ],
   },
   {
     company: "Spot Parking",
     location: "Provo, UT",
-    position: "Backend Engineer Intern",
+    position: "Cloud Engineer Intern",
     duration: "Jan 2025 – May 2025",
     responsibilities: [
-      "Worked with founders to design a relational database schema for scalability and migrated a NoSQL DynamoDB database to an AWS RDS MySQL instance, using Python scripts for data ETL.",
+      "Worked with owners to design a relational database schema for scalability and migrated a NoSQL DynamoDB database to an AWS RDS MySQL instance, using Python scripts for data ETL",
     ],
   },
   {
